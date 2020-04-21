615a277b3774c1277833994bbcd5f318bf158a8e
nisaprmst <nisaprmst@gmail.com>
2020-04-22 04:09:14 +0700

    add all routes and pages

--- src/main.py
+++ src/main.py
@@ -1,9 +1,65 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+from flask import request, redirect
+from werkzeug.utils import secure_filename
+from lib.regex_lib import search_keyword_regex, extract
+from lib.kmp_lib import search_keyword_kmp
+from lib.boyer_moore_lib import search_keyword_bm
+from nltk.tokenize import sent_tokenize
+import os
+
+# from werkzeug import secure_filename
+app = Flask(__name__, template_folder='view')
+# Create a directory in a known location to save files to.
+uploads_dir = os.path.join(os.path.dirname(app.instance_path), 'text')
+os.makedirs(uploads_dir, exist_ok=True)
+
 
 @app.route('/')
-def hello_world():
-   return 'Hello'
+def index():
+   return render_template('index.html')
+
+@app.route('/beranda')
+def beranda():
+   return render_template('index.html')
+
+@app.route('/perihal')
+def perihal():
+   return render_template('perihal.html')
+
+@app.route('/ekstrak', methods=["GET", "POST"])
+def ekstrak():
+   return render_template('ekstrak.html')
+
+@app.route('/hasil', methods=["GET", "POST"])
+def hasil():
+   if request.method == "POST":
+      keyword = request.form
+      data = request.files['file']
+      # data.save(secure_filename(data.filename))
+      data.save(os.path.join(uploads_dir, "test.txt"))
+      filename = 'text/test.txt'
+      file = open(filename, "r")
+      text = file.read()
+      print(text)
+
+      data = sent_tokenize(text)
+      key = ''
+      for a, b in keyword.items():
+         key = b
+      print(key)
+      res = search_keyword_bm(data, key)
+
+      extraction = extract(res, key)
+      print(extraction)
+      error = 'Keyword tidak ditemukan'
+      if len(extraction) != 0:
+         error = ''
+         for i in range(len(extraction)):
+             print('Jumlah:', extraction[i][1])
+             print('Waktu: ', extraction[i][0])
+      return render_template('hasil.html', keyword = keyword, error = error, extraction = extraction, res = res, len = len(extraction))
+
 
 if __name__ == '__main__':
    app.run(debug=True)
